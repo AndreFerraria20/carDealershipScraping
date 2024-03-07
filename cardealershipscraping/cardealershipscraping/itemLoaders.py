70b43125fd8d40ea3dae9844297e83d218c9b376
André <2181206@my.ipleiria.pt>
2024-03-07 12:34:40 +0000

    ItemLoader with simple mileage cleaning and  transformation

--- cardealershipscraping/cardealershipscraping/itemLoaders.py
+++ cardealershipscraping/cardealershipscraping/itemLoaders.py
@@ -6,27 +6,40 @@ from itemloaders.processors import MapCompose, TakeFirst
 
 
 def clean_price(value):
-    print("Cleaning price",value)
     c=CurrencyConverter(fallback_on_missing_rate=True)
     euro_list=['€','euro','eur']
     value = value.replace('.', '').replace(' ','')
-    print(value)
     for i in euro_list:        
         if op.contains(value, i):
-            value.split(i)[-1]
+            value.replace(i,"")
             return value
 
     zloti_list=['zł','PLN']
     value = value.replace('.', '')
     for i in zloti_list:
         if op.contains(value, i):
-            value.split(i)[-1]
+            value.replace(i,"")
             c.convert(value,'PLN','EUR')
             return value
 
     return float(value)
 
-
+def clean_mileage(value):
+    value = value.replace('.', '').replace(' ','')
+    km_list=['km','kilometros']
+    for i in km_list:        
+            if op.contains(value, i):
+                print("true")
+                value=value.replace(i,"")
+                return float(value)
+    mileage_list=['miles','mileage']
+    for i in mileage_list:        
+            if op.contains(value, i):
+                value=value.replace(i,"")
+                value=float(value)*1.609344
+                return value
+    return value
 class CarDealershipItemLoader(ItemLoader):
     default_input_processor=TakeFirst()
     price_in=MapCompose(clean_price)
+    mileage_in=MapCompose(clean_mileage)
